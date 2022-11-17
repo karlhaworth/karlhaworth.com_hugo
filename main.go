6ea8e50cd30182e241716357b35b0c0638980f11
karlhaworth <github@karlhaworth.com>
2022-11-17 13:16:57 -0500

    update

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
-// Command pdf is a chromedp example demonstrating how to capture a pdf of a
-// page.
+// Package main ...
 package main
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/cdproto/emulation"
-	"github.com/chromedp/cdproto/page"
-	"github.com/chromedp/chromedp"
+	"github.com/go-rod/rod"
 )
 
 func main() {
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
 	// get local file
 	path, err := os.Getwd()
 	if err != nil {
@@ -28,36 +18,6 @@ func main() {
 
 	filepath := path + "/public/index.html"
 
-	// capture pdf
-	var buf []byte
-	if err := chromedp.Run(ctx, printToPDF("file://"+filepath, &buf)); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := ioutil.WriteFile("./public/karl_haworth-resume.pdf", buf, 0o644); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("wrote karl_haworth-resume.pdf")
-}
-
-// print a specific pdf page.
-func printToPDF(urlstr string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-
-			return emulation.SetDefaultBackgroundColorOverride().
-				// green
-				WithColor(&cdp.RGBA{R: 0, G: 255, B: 0, A: 0}).
-				Do(ctx)
-		}),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			buf, _, err := page.PrintToPDF().WithMarginTop(.15).WithMarginBottom(.15).WithMarginRight(.15).WithMarginLeft(.15).WithPrintBackground(true).Do(ctx)
-			if err != nil {
-				return err
-			}
-			*res = buf
-			return nil
-		}),
-	}
+	rod.New().MustConnect().MustPage("file://" + filepath).MustWaitLoad().MustPDF("./public/karl_haworth-resume.pdf")
+	fmt.Println("wrote ./public/karl_haworth-resume.pdf")
 }
