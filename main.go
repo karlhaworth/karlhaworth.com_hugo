b0bbe0698b76b813e044acd7378772c6816d8145
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-04-08 09:19:58 -0400

    fix: update margins (#66)

--- main.go
+++ main.go
@@ -42,10 +42,10 @@ func main() {
 	pdf, _ := page.PDF(&proto.PagePrintToPDF{
 		PaperWidth:      gson.Num(8.5),
 		PaperHeight:     gson.Num(11),
-		MarginTop:       gson.Num(0.15),
-		MarginBottom:    gson.Num(0.15),
-		MarginRight:     gson.Num(0.15),
-		MarginLeft:      gson.Num(0.15),
+		MarginTop:       gson.Num(0.25),
+		MarginBottom:    gson.Num(0.25),
+		MarginRight:     gson.Num(0.25),
+		MarginLeft:      gson.Num(0.25),
 		Scale:           gson.Num(0.95),
 		PrintBackground: true,
 	})
